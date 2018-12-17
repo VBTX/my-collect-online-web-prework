82fde841829d0acc8857e27926b10d982d8ab29a
Victoria B <viktorija.bourassa@gmail.com>
2018-12-17 20:27:08 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,18 @@
 def my_collect(array)
-  if block_given?
+    if block_given?
     i = 0
   while i < array.length
     yield (array[i].split(" ").first)
+    i = i + 1
+  end
+end
+end
+
+def my_collect(array)
+    if block_given?
+    i = 0
+  while i < array.length
+    yield (array[i].upcase)
     i = i + 1
   end
 end

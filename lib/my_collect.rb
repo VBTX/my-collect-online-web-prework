326c6747ce334ed5d6912cb4234c85c5e558117a
Victoria B <viktorija.bourassa@gmail.com>
2018-12-17 20:43:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,9 @@
 def my_collect(array)
+  if block_given?
     i = 0
-    arr2 =[]
-    while i < array.length
-      a = array[i].split(" ").first
-      arr2 << yield a
-      i += 1
-    end
+  while i < array.length
+    yield (array[i].split(" ").first)
+    i = i + 1
+  end
 end
-  arr2
 end

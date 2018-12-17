d8547dd5c92238512adad810b665b991e3b78a75
Victoria B <viktorija.bourassa@gmail.com>
2018-12-17 20:42:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,8 @@ def my_collect(array)
     i = 0
     arr2 =[]
     while i < array.length
-      arr2 << yield (array[i].split(" ").first)
+      a = array[i].split(" ").first
+      arr2 << yield
       i += 1
 end
   arr2

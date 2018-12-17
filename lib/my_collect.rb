e0a1401435396ce920a7adff55cb285b3298aedb
Victoria B <viktorija.bourassa@gmail.com>
2018-12-17 20:32:33 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,9 +3,8 @@ def my_collect(array)
     i = 0
   while i < array.length
     arr2 =[]
-    yield (array) {|i| arr2.push([i].split(" ").first)}
-    i = i + 1
-  end
+    yield (array) {|i| arr2.push([i].split(" ").first)
+    i = i + 1 }
   arr2
 end
 end
